b12e122ee037e8c7a1fb78117ef90a29e8d5b4bc
nice2pandu <103422623+nice2pandu@users.noreply.github.com>
2022-10-11 20:13:11 +0530

    Update CalculatorTest.java

--- src/test/java/in/javahome/myweb/controller/CalculatorTest.java
+++ src/test/java/in/javahome/myweb/controller/CalculatorTest.java
@@ -2,7 +2,6 @@ package in.javahome.myweb.controller;
 
 import junit.framework.Assert;
 import junit.framework.TestCase;
-import java.util.*;
 
 public class CalculatorTest extends TestCase {
 	Calculator cal = new Calculator();
@@ -12,8 +11,5 @@ public class CalculatorTest extends TestCase {
 	public void testMultiply(){
 		Assert.assertEquals(cal.multiply(10, 20), 200);
 	}
-	public static void main( String[] args )
-	{
-	List<String> stringList = new ArrayList<String>();
-	}
+	
 }
